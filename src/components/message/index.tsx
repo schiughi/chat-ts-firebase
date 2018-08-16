d9ae5de25846d787baa6f36d1dc1ce468b24560d
schiughi <schiughi@gmail.com>
2018-08-16 22:05:02 +0900

    add message component

---
+++ src/components/message/index.tsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+import * as styles from "./style.css"
+
+interface MessageProps {
+  userIcon: string
+  userName: string
+  text?: string
+}
+
+export const Message: React.SFC<MessageProps> = props => (
+  <div className={styles.message}>
+    <img className={styles.icon} src={props.userIcon} />
+    <div className={styles.box}>
+      <p className={styles.name}>@{props.userName}</p>
+      <p className={styles.text}>{props.text}</p>
+    </div>
+  </div>
+)
---
+++ src/components/message/style.css.d.ts
@@ -0,0 +1,5 @@
+export const message: string
+export const icon: string
+export const box: string
+export const name: string
+export const text: string
